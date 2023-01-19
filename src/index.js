f6dd489ab02a3411ed20f43e3fc2d5d424dab9e1
phamchidung <dung6app@gmail.com>
2023-01-19 11:12:46 +0700

    test

--- src/index.js
+++ src/index.js
@@ -33,6 +33,6 @@ app.set('views', path.join(__dirname, 'resources/views')); // config 'views' fol
 // routes init
 route(app);
 
-                app.listen(port, () => {
+                   app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
