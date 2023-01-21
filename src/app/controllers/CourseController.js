291e41e86e6a16ae06bf26a88358750c9439eb63
phamchidung <dung6app@gmail.com>
2023-01-21 23:32:56 +0700

    soft delete

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -31,12 +31,12 @@ class CourseController {
 
     // [POST] /courses/store
     store(req, res, next) {
-        const formData = req.body;
+        const formData = { ...req.body };
         formData.image = `https://img.youtube.com/vi/${formData.videoId}/sddefault.jpg`;
         const course = new Course(formData);
         course
             .save()
-            .then(() => res.redirect('/'))
+            .then(() => res.redirect('/me/stored/courses'))
             .catch((error) => {});
     }
 
@@ -44,6 +44,27 @@ class CourseController {
     update(req, res, next) {
         Course.updateOne({ _id: req.params.id }, req.body)
             .then(() => res.redirect('/me/stored/courses'))
+            .catch(next);
+    }
+
+    // [DELETE] /courses/:id
+    delete(req, res, next) {
+        Course.delete({ _id: req.params.id })
+            .then(() => res.redirect('back'))
+            .catch(next);
+    }
+
+    // [DELETE] /courses/:id/force
+    forceDelete(req, res, next) {
+        Course.deleteOne({ _id: req.params.id })
+            .then(() => res.redirect('back'))
+            .catch(next);
+    }
+
+    // [PATCH] /courses/:id/restore
+    restore(req, res, next) {
+        Course.restore({ _id: req.params.id })
+            .then(() => res.redirect('back'))
             .catch(next);
     }
 }
--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 async function connect() {
     try {
+        mongoose.set('strictQuery', false);
         await mongoose.connect('mongodb://127.0.0.1/f8_education_dev');
         console.log('connect successfully!!!');
     } catch (error) {
